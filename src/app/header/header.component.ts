2f0859afcb3472e1557c511a6ed97ca3d05da152
enigma341 <shubhamsingh7298@gmail.com>
2019-01-25 11:09:11 +0530

    added logout btn

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private authService: AuthService) { }
+  ngOnInit() { }
 }
